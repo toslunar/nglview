6776850da0b3cb12835b9469a0423901fde532c0
Toshiki Kataoka <kataoka@preferred.jp>
2023-05-23 10:35:52 +0900

    restore python exports

--- nglview/__init__.py
+++ nglview/__init__.py
@@ -1,11 +1,28 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# Copyright (c) Alexander S. Rose, Hai Nguyen.
-# Distributed under the terms of the Modified BSD License.
+import warnings
 
 from .example import ExampleWidget
 from ._version import __version__, version_info
+# for doc
+from . import adaptor, datafiles, show, widget
+from .adaptor import *
+from .base_adaptor import *
+# TODO: do not use import *
+# interface
+from .config import BACKENDS
+from .data_source import DatasourceRegistry
+from .show import *
+# utils
+from .utils import js_utils, widget_utils
+from .widget import NGLWidget, write_html
+
+with warnings.catch_warnings():
+    warnings.simplefilter("ignore")
+
+# Register nbextension
+
 
 def _jupyter_labextension_paths():
     """Called by Jupyter Lab Server to detect if it is a valid labextension and
@@ -47,3 +64,7 @@ def _jupyter_nbextension_paths():
         'dest': 'nglview',
         'require': 'nglview/extension'
     }]
+
+
+__all__ = ['NGLWidget', 'write_html'
+           ] + widget.__all__ + adaptor.__all__ + show.__all__
--- nglview/_frontend.py
+++ nglview/_frontend.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# Copyright (c) Alexander S. Rose, Hai Nguyen.
-# Distributed under the terms of the Modified BSD License.
-
 """
 Information about the frontend package of the widgets.
 """
 
 module_name = "nglview-js-widgets"
 module_version = "^0.1.0"
+
+# For compatibility (nglview <= 3.0.4)
+__frontend_version__ = module_version
