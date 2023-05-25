318d41fc661935a533865a1d4d72de3a0dbc632d
Toshiki Kataoka <kataoka@preferred.jp>
2023-05-25 14:32:15 +0900

    Fix nbextenion path in tests

--- nglview/tests/test_widget.py
+++ nglview/tests/test_widget.py
@@ -731,9 +731,9 @@ def test_trajectory_show_hide_sending_cooridnates():
 
 def test_existing_js_files():
     from glob import glob
-    jsfiles = glob(os.path.join(os.path.dirname(nv.__file__), 'static', '*js'))
+    jsfiles = glob(os.path.join(os.path.dirname(nv.__file__), 'nbextension', '*js'))
     mapfiles = glob(
-        os.path.join(os.path.dirname(nv.__file__), 'static', '*map'))
+        os.path.join(os.path.dirname(nv.__file__), 'nbextension', '*map'))
 
     assert len(jsfiles) == 2
     assert len(mapfiles) == 1
